03404beb59ade2fe8bf5be88bb8b6906fe300935
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-03-16 13:20:57 +0100

    Update HomeWorkHelloWorld1.java

--- HomeWorkHelloWorld1.java
+++ HomeWorkHelloWorld1.java
@@ -20,7 +20,6 @@ public class HomeWorkHelloWorld1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.print("Hello everybody ");
-		System.out.println("and my friends");
+		System.out.print("Hello everybody and my friends");
 	}
 }
